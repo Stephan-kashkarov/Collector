c861532383ddc26933dee73b2eaacb352d7b57d9
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-03-08 13:48:24 +1100

    worked with the js

--- main/js/main.js
+++ main/js/main.js
@@ -1,3 +1,17 @@
+function Article(tag, title, img, desc) {
+	$(tag).append(<h1>title</h1>);
+	$(tag).append(<img>img</img>);
+	$(tag).append(<p>desc</p>);
+
+}
+
 $(document).ready(function(e) {
-	
+	$(".edit").submit(function(e){
+		event.preventDefault();
+		
+		var new_img = $(".img").val()
+		var new_title = $(".new-title").val()
+		var new_desc = $(".new-desc").val()
+		Article()
+	});
 });

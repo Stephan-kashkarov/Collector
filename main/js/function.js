db3fd604edbb1e9aac6b38ab364d92eee1665717
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-03-13 18:31:34 +1100

    Extensive overhaul beginning!

--- main/js/function.js
+++ main/js/function.js
@@ -1,48 +1,20 @@
 var articles = [];
 
-class article {
-	constructor(id, title, desc, img = "") {
-		this.id = id;
-		this.title = title;
-		this.desc = desc;
-		this.img = img;
-	};
-	listify() {
-		return {
-			title: this.title,
-			img: this.img,
-			desc: this.desc
-		};
-	};
-	append_article() {
-		var formatted = this.listify();
-		articles.push(formatted);
-		console.log("Article Pushed");
-	};
-	id() {
-		return this.id
-	};
-};
+class post {
+	constructor(title, desc, img = "", tags, id) {
+		this.title = title
+		this.desc = desc
+		this.img = img
+		this.tags = tags
+		this.id = id
+	}
 
-function print_posts() {
-	$(".article").remove();
-	console.log("Refreshing posts!");
-	if(articles != "") {
-		console.log("Articles not empty!");
-		for(var i = 0; i < articles.length; i++) {
-			$(".main").append([articles[i]].map(article_template).join(""));
-			console.log(articles[i], "Printed");
-		};
-	};
-	console.log("Posts refreshed!");
-};
-
-function prepareUpload(event) {
-	files = event.target.files;
-};
-
-class database {
-	constructor(ajaxdb) {
-		this.db = ajaxdb;
-	};
-};
+	function listform() {
+		objform = {
+			"title": this.title,
+			"img": this.img,
+			"desc": this.desc
+		}
+		return objform;
+	}
+}
--- main/js/main.js
+++ main/js/main.js
@@ -13,44 +13,13 @@ $(document).ready(function (e) {
 		}
 	});
 
-
-
-	console.log("Contents of ajaxed file(s):",db.responseText);
 	var items = JSON.parse(db.responseText);
 	for (var i in items) {
-		console.log("Appending and printing:", items[i]);
-		var articleid = "article" + String(items[i].id);
-		console.log("Id of article:", articleid);
-		var articleid = new article (items[i].id, items[i].title, items[i].desc, items[i].img);
-		console.log(articleid, articleid.listify());
-		articleid.append_article();
-		print_posts(articles);
+		console.log(items[i]);
 	}
 
-	if(localStorage.lenght) {
-		for(var i=0; i < localStorage.length; i++) {
-			console.log("localStorage: " + localStorage.key(i));
-		}
-	}
-	$(".edit").submit(function (e) {
-		event.preventDefault();
-		var id = articles.length + 1;
-		var idnum = articles.length + 1;
-		var articleid = "article" + String(id);
-		var new_img = $(".img").val();
-		var new_title = $(".new-title").val();
-		var new_desc = $(".new-desc").val();
-		var id = new article (id, new_title, new_desc, new_img);
-		console.log(id, id.listify());
-		id.append_article();
-		print_posts();
-		$(".new-desc").val("");
-		$(".new-title").val("");
-		$(".new-img").val("");
-		window.localStorage.clear();
-		window.localStorage.setItem(1, "articles");
-	});
+
 	$(".refresh-posts").click(function(e){
-		print_posts();
+
 	});
 });

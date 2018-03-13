c2777a3d24954d5a217b9080cbfdae45768ac9ab
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-03-13 14:42:54 +1100

    Revert "did things"
    This reverts commit b4baa6523f3a70557641064f9cefcb1866030bcc.

--- main/js/function.js
+++ main/js/function.js
@@ -25,3 +25,44 @@ class article {
 		return this.id;
 	};
 };
+<<<<<<< HEAD
+=======
+
+function db() {
+	var database = window.localStorage.getItem("db");
+	console.log("Unstringified DB:", database);
+	var database = JSON.stringify(database);
+	console.log("Stringified DB:", database);
+	$.ajax({
+		url: "database/posts.json",
+		async: false,
+		method: "POST",
+		data: database
+	});
+};
+
+function print_posts() {
+	$(".article").remove();
+	window.localStorage.setItem("db", articles);
+	db()
+	console.log("Refreshing posts!");
+	if(articles != "") {
+		console.log("Articles not empty!");
+		for(var i = 0; i < articles.length; i++) {
+			$(".main").append([articles[i]].map(article_template).join(""));
+			console.log(articles[i], "Printed");
+		};
+	};
+	console.log("Posts refreshed!");
+};
+
+function prepareUpload(event) {
+	files = event.target.files;
+};
+
+class database {
+	constructor(ajaxdb) {
+		this.db = ajaxdb;
+	};
+};
+>>>>>>> parent of b4baa65... did things
--- main/js/main.js
+++ main/js/main.js
@@ -1,3 +1,43 @@
 $(document).ready(function (e) {
 
+<<<<<<< HEAD
+=======
+
+
+	console.log("Contents of ajaxed file(s):",db.responseText);
+	var items = JSON.parse(db.responseText);
+	for (var i in items) {
+		console.log("Appending and printing:", items[i]);
+		var articleid = "article" + String(items[i].id);
+		console.log("Id of article:", articleid);
+		var articleid = new article (items[i].id, items[i].title, items[i].desc, items[i].img);
+		articleid.append_article();
+		print_posts(articles);
+	}
+
+	if(localStorage.lenght) {
+		for(var i=0; i < localStorage.length; i++) {
+			console.log("localStorage: " + localStorage.key(i));
+		}
+	}
+	$(".edit").submit(function (e) {
+		event.preventDefault();
+		var id = articles.length + 1;
+		var idnum = articles.length + 1;
+		var articleid = "article" + String(id);
+		var new_img = $(".img").val();
+		var new_title = $(".new-title").val();
+		var new_desc = $(".new-desc").val();
+		var id = new article (id, new_title, new_desc, new_img);
+		console.log(id, id.listify());
+		id.append_article();
+		print_posts();
+		$(".new-desc").val("");
+		$(".new-title").val("");
+		$(".new-img").val("");
+	});
+	$(".refresh-posts").click(function(e){
+		print_posts();
+	});
+>>>>>>> parent of b4baa65... did things
 });

61d34da324a68413f303342657c12876654fd0f7
vitalykashkarov <vkashkarov@gmail.com>
2018-03-18 12:30:10 +1100

    Fixed JSON generation

--- scripts/parser_tsv.py
+++ scripts/parser_tsv.py
@@ -3,7 +3,7 @@ import json
 
 # VARIABLE DEFINITION
 x = 0
-parsed_data = {}
+parsed_data = []
 artist_cache = {}
 art_cache = {}
 art_list = []
@@ -13,12 +13,11 @@ def cascade_art(art_list, art_cache):
 	art_list.append(art_cache)  # APPEND DICTIONARY OF ARTWORK TO LIST
 
 
-def cascade_all(last_name, line, artist_cache, parsed_data, art_cache,
-				art_list):
+def cascade_all(artist_cache, parsed_data, art_cache, art_list):
 	cascade_art(art_list, art_cache)  # APPEND THE LAST ARTWORK
-	artist_cache[
-		"art"] = art_list  # ADDS ART LIST AS A ATTRIBUTE OF THE ARTIST
-	parsed_data[last_name] = artist_cache  # ADDS THE ARTIST TO THE FINAL LIST
+	if len(art_list) > 1:
+		artist_cache["art"] = art_list  # ADDS ART LIST AS A ATTRIBUTE OF THE ARTIST
+		parsed_data.append(artist_cache)  # ADDS THE ARTIST TO THE FINAL LIST
 
 
 def next_field(list, pos):
@@ -64,7 +63,7 @@ def make_art(art_cache, title, date, technique, location, url, form, painting_ty
 art_index = 0
 with open('../data/artistcsvdata.csv', newline='\n', encoding='utf-8') as f:
 	data = f.read()
-	lines = data.split('\n')
+	lines = data.split('\r\n')
 
 for line in lines:
 	if art_index > 0:
@@ -72,13 +71,14 @@ for line in lines:
 		num_fields = len(fields)
 		pos = 0
 		name, pos = next_field(fields, pos)
-		if last_name == "":
-			last_name = name
 		if len(name) == 0:
+			art_index += 1 
 			continue
+		# if last_name == "":
+		# 	last_name = name
 		if name != last_name:
-			cascade_all(last_name, line, artist_cache, parsed_data, art_cache, art_list)
-			name = last_name
+			cascade_all(artist_cache, parsed_data, art_cache, art_list)
+			last_name = name
 			art_cache = {}
 			art_list = []
 			artist_cache = {}
@@ -150,10 +150,11 @@ for line in lines:
 			if (pos < num_fields):
 				timeframe, pos = next_field(fields, pos)
 
-			make_art(art_cache, title, date, technique, location, url, form, painting_type, art_index)
 			cascade_art(art_list, art_cache)
+			make_art(art_cache, title, date, technique, location, url, form, painting_type, art_index)
+			
 			art_cache = {}
 	art_index += 1
 
 with open("../data/artists.json", "w") as outfile:  # SETS JSON FILE AS WRITE
-	json.dump(parsed_data, outfile)  # DUMPS DATA TO JSON FILE
+	json.dump(parsed_data, outfile, indent=4)  # DUMPS DATA TO JSON FILE

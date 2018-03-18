c096f1551e53bc81f1078d2015e94bc818ab917b
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-03-18 20:43:28 +1100

    made two more functions

--- main/js/function.js
+++ main/js/function.js
@@ -23,3 +23,23 @@ function make_data_list(artist_dict) {
 		$("#artist_list").html([artist_dict[i]].map(autocomplete_template).join(""));
 	}
 }
+
+function sort(artist_dict, artist_list, search) {
+	for (var i = 0; i < artist_list.length; i++) {
+		if (artist_list[i].includes(search)) {
+			print_artist(artist_dict[i], i);
+		}
+	}
+}
+
+function print_artist(object, index) {
+	object = $.ajax({
+		method: "GET",
+		url: artist_list[index][1]
+		error: function(xhr){
+			console.log("Getting " + artist_list[index][0] + "'s profile. Error: " + xhrstatus);
+		}
+	});
+	object = object.responseText;
+
+}

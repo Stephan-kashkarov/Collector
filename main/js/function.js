c44dac65bc1dbd75f78b42cd6ad5036160276eec
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-03-29 09:03:08 +1100

    restructured artist_lite class

--- main/js/function.js
+++ main/js/function.js
@@ -9,7 +9,30 @@ var thing;
 var piece;
 var obj;
 
+class artist_lite {
+	constructor(obj){
+		this.obj = obj;
+		this.name = obj["name"];
+		this.school = obj["school"];
+		this.timeframe = obj["timeframe"];
+	}
 
+	load() {
+		get_url(this.name, artist_list)
+	}
+}
+
+get_url(name, artist_list){
+	for (var i = 0; i < artist_list.length; i++) {
+		console.log("Are", name, "and", artist_list[i]["name"], "the same thing?");
+		if (artist_list[i]["name"] == name) {
+			console.log("Yes!");
+			return artist_list[i]["url"];
+		} else {
+			console.log("No! ;(");
+		}
+	}
+}
 
 // STACKOVERFLOW PURE JS SOLUTION
 function hasClass(elem, className) {
